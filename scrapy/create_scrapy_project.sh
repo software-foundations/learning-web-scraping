d3e6c83b4abf358c3ff61d3e6073f746cbc4142d
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-04-15 19:21:18 -0300

    (courses, and create_scrapy_project.sh, at /scrapy) -> 32: Creating scrapy project

--- scrapy/create_scrapy_project.sh
+++ scrapy/create_scrapy_project.sh
@@ -1,1 +1,9 @@
+# create a project
 scrapy startproject courses
+
+# create a spyder
+cd courses
+scrapy genspider coursera https://www.coursera.org/browse\?language\=pt
+
+# run a spyder
+scrapy crawl coursera
